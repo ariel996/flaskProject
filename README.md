9a27a62771fa86db108d47993123adc53b1f5873
ariel nana <arieldeguilique@hotmail.com>
2022-09-13 23:39:09 +0100

    login done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # flaskProject
+# How to install the dependencies
+1. Run the following command: `pip install -r requirements.txt`
--- app.py
+++ app.py
@@ -1,15 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+from requests import get
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def login():  # put application's code here
-    return render_template('index.html')
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    username = request.form['username']
+    password = request.form['password']
+    url = 'https://blossom.nvidia.com/jenkins/api/v1/projects/instances/list/'
+    headers = {
+        'Content-type': 'application/json'
+    }
+    response = get(url, auth=(username, password), headers=headers)
+    output_json = response.json()
+    if response.status_code == 200:
+        return redirect(url_for('homepage', response=output_json))
+    else:
+        return redirect(url_for('login'))
+
+
 
 @app.route('/homepage')
 def homepage():
-    return 'homepage'
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
